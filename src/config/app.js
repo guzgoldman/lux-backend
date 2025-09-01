af55f32d62bed152b4f1d612e4caefd89a65ddbe
roGitHubAcc <romilu85@gmail.com>
2025-09-01 19:01:15 -0300

    se agregó calculo de  estadisticas

--- src/config/app.js
+++ src/config/app.js
@@ -24,7 +24,7 @@ const app = express();
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://192.168.0.213:5173",
+    origin: "http://192.168.100.133:5173",
     credentials: true,
   })
 );
