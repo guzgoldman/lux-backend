d211ec08fe45f513e54199fd803923f89e7fdb85
Héctor Guzmán <107734309+guzgoldman@users.noreply.github.com>
2025-09-11 15:54:14 -0300

    feat: add initial structure for examenFinal controller and routes

---
+++ src/modules/admin/gestionExamenFinal/examenFinalController.js
@@ -0,0 +1,14 @@
+const {
+  Materia,
+  MateriaPlan,
+  Correlativa,
+  InscripcionExamenFinal,
+  AsistenciaExamenFinal,
+  ExamenFinal,
+  InscripcionMateria,
+  MateriaPlanCicloLectivo,
+  HistorialAsistenciaExamenFinal,
+  HistorialInscripcionExamenFinal,
+  CalificacionCuatrimestre,
+} = require("../../../models");
+
